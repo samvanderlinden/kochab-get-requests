d5bdce986cb5259e474bdd9e71337829cf156edf
Sam Vanderlinden <samvanderlinden6@gmail.com>
2018-04-18 16:23:30 -0500

    add random quote on click, and appended list of quotes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,26 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jquery sourced');
+    $.ajax({
+        type: 'GET',
+        url: '/all-quotes'
+    })
+    .then(function(randomNumber) {
+        console.log(randomNumber);
+        $('#quotes').append('<li>' + randomNumber[0].quote + '</li>');
+        $('#quotes').append('<li>' + randomNumber[1].quote + '</li>');
+        $('#quotes').append('<li>' + randomNumber[2].quote + '</li>');
+    })
+
+    $('#quoteButton').on('click', function(){
+        console.log('button clicked');
+            $.ajax({
+                type: 'GET',
+                url: '/quote'
+            })
+            .then(function(randomNumber) {
+                console.log(randomNumber)
+                $('#randomQuote').text(randomNumber.quote);
+            })
+    })
 }
