f7f8fb88ec13d6eab7f4bea1fe76edf23bd8f451
Sam Vanderlinden <samvanderlinden6@gmail.com>
2018-04-19 18:36:56 -0500

    add new quote entered into text area

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,26 +4,59 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jquery sourced');
+    getAllQuotes();
+    $('#newQuoteButton').on('click', addNew);
+}
+
+function getAllQuotes() {
     $.ajax({
-        type: 'GET',
+        method: 'GET',
         url: '/all-quotes'
     })
-    .then(function(randomNumber) {
-        console.log(randomNumber);
-        $('#quotes').append('<li>' + randomNumber[0].quote + '</li>');
-        $('#quotes').append('<li>' + randomNumber[1].quote + '</li>');
-        $('#quotes').append('<li>' + randomNumber[2].quote + '</li>');
-    })
+        .then(function (randomNumber) {
+            console.log(randomNumber);
+            $('#quotes').empty();
+            randomNumber.forEach(function (randomNumbers) {
+                $('#quotes').append(
+                    '<li>' + randomNumbers.quote + ' - ' + randomNumbers.author + '</li>' 
+                );
+            });
+            // $('#quotes').append('<li>' + randomNumber[0].quote + '</li>');
+            // $('#quotes').append('<li>' + randomNumber[1].quote + '</li>');
+            // $('#quotes').append('<li>' + randomNumber[2].quote + '</li>');
+        });
 
-    $('#quoteButton').on('click', function(){
+    $('#quoteButton').on('click', function () {
         console.log('button clicked');
-            $.ajax({
-                type: 'GET',
-                url: '/quote'
-            })
-            .then(function(randomNumber) {
+        $.ajax({
+            type: 'GET',
+            url: '/quote'
+        })
+            .then(function (randomNumber) {
                 console.log(randomNumber)
                 $('#randomQuote').text(randomNumber.quote);
-            })
+            });
+    });
+
+}
+
+// this is the start of POST request
+
+function addNew() {
+
+    console.log('newQuote button clicked');
+    const newQuote = {
+        quote: $('textarea').val(),
+        author: $('#authorInput').val()
+    };
+    console.log('new quote object:', newQuote);
+    $.ajax({
+        method: 'POST',
+        url: '/add-quote',
+        data: newQuote
     })
+        .then(function (quotes) {
+            console.log('new random quote:', quotes);
+            getAllQuotes();
+        });
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 console.log('Hey there')
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 const quotes_data = require('../quotes_data');
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/all-quotes', function(req, res){ // '/all-quotes' is called a 'route'
     res.send(quotes_data);
@@ -32,6 +35,12 @@ app.get('/quote', function(req, res){
 //     res.send(quotes_data[0]);
     
 // });
+
+app.post('/add-quote', function(req, res){
+    console.log(req.body); //whatever data we send on our post is req.body in our server
+    quotes_data.push(req.body);
+    res.sendStatus(200);
+});
 
 app.listen(PORT, function (){ // this function waits for server to run and then the function calls back the 'console.log'
     console.log(`listening to port, ${PORT}`)
